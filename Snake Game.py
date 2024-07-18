3ed21c467d7a8a71a41c4b20dbd49cae0da45bab
Ng Jian Wei <166774579+w3i-jn@users.noreply.github.com>
2024-07-18 09:36:35 +0800

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -62,7 +62,7 @@ score = 0
 # Game Over
 def game_over():
     my_font = pygame.font.SysFont('times new roman', 90)
-    game_over_surface = my_font.render('YOU DIED', True, red)
+    game_over_surface = my_font.render('Gameover', True, red)
     game_over_rect = game_over_surface.get_rect()
     game_over_rect.midtop = (frame_size_x/2, frame_size_y/4)
     game_window.fill(black)
